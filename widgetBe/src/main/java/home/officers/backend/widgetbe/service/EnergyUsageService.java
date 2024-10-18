3aa3a90458766f37c6db518500334a25347fc758
Kamil00o <laganowski.kamil@gmail.com>
2024-10-18 13:56:16 +0200

    computeConsumedEnergyCost basic code
    Signed-off-by: Kamil00o <laganowski.kamil@gmail.com>

--- widgetBe/src/main/java/home/officers/backend/widgetbe/service/EnergyUsageService.java
+++ widgetBe/src/main/java/home/officers/backend/widgetbe/service/EnergyUsageService.java
@@ -1,7 +1,25 @@
 package home.officers.backend.widgetbe.service;
 
+import home.officers.backend.widgetbe.model.domain.Customer;
+import home.officers.backend.widgetbe.model.domain.EnergyUsageLog;
+import home.officers.backend.widgetbe.repository.EnergyUsageLogRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 @Service
+@RequiredArgsConstructor
 public class EnergyUsageService {
+
+    private final EnergyUsageLogRepository repository;
+
+    public TreeMap<String, Double> computeConsumedEnergyCost(Customer customer) {
+        List<EnergyUsageLog> energyUsageLog = repository.findByCustomerId(customer.getId())
+                .orElse(new ArrayList<EnergyUsageLog>());
+
+        return new TreeMap<>();
+    }
 }
