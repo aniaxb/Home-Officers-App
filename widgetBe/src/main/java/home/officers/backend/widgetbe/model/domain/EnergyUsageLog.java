7682bd4e4754600fd1a451b71e027dee665c2979
Damian Biskupski <ddamian2057@gmail.com>
2024-10-18 13:08:36 +0200

    Update EnergyUsageLog.java

--- widgetBe/src/main/java/home/officers/backend/widgetbe/model/domain/EnergyUsageLog.java
+++ widgetBe/src/main/java/home/officers/backend/widgetbe/model/domain/EnergyUsageLog.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @Table(name = "energy_usage_log")
 public class EnergyUsageLog {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
