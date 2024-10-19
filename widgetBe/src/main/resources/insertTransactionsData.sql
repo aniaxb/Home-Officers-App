c6b60f3307b9ddf87605dcd6adfab49250d8e52c
Kamil00o <laganowski.kamil@gmail.com>
2024-10-19 13:50:32 +0200

    Fix for insertTransactionsData.sql
    Signed-off-by: Kamil00o <laganowski.kamil@gmail.com>

--- widgetBe/src/main/resources/insertTransactionsData.sql
+++ widgetBe/src/main/resources/insertTransactionsData.sql
@@ -1,19 +1,34 @@
--- Inserting 600 records
-INSERT INTO transactions (customer_name, transaction_type, transaction_value, currency_code, transaction_date)
-VALUES
--- 480 (80%) Digital Operation transactions
--- We use `RAND()` to generate random data for transaction value and to randomly pick a currency.
--- Dates are within September 2024 using the `DATE_FORMAT()` function to ensure the proper range.
+DELIMITER $$
+CREATE PROCEDURE insert_random_transactions()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE transaction_type VARCHAR(255);
 
--- Insert 480 digital operations
--- Generating random dates in September 2024
-('Customer 1', 'digital operation', ROUND(100 + RAND() * 900, 2),
- ELT(FLOOR(1 + RAND() * 4), 'USD', 'PLN', 'EUR', 'GBP'),
- CONCAT('2024-09-', LPAD(FLOOR(1 + RAND() * 30), 2, '0'), ' ', LPAD(FLOOR(RAND() * 24), 2, '0'), ':', LPAD(FLOOR(RAND() * 60), 2, '0'), ':', LPAD(FLOOR(RAND() * 60), 2, '0'))),
+    WHILE i <= 600 DO
+            -- Determine transaction type: 80% 'digital operation', 20% 'cash'
+            IF i <= 480 THEN
+                SET transaction_type = 'digital operation';
+            ELSE
+                SET transaction_type = 'cash';
+            END IF;
 
--- Insert 120 cash transactions
-('Customer 1', 'cash', ROUND(100 + RAND() * 900, 2),
- ELT(FLOOR(1 + RAND() * 4), 'USD', 'PLN', 'EUR', 'GBP'),
- CONCAT('2024-09-', LPAD(FLOOR(1 + RAND() * 30), 2, '0'), ' ', LPAD(FLOOR(RAND() * 24), 2, '0'), ':', LPAD(FLOOR(RAND() * 60), 2, '0'), ':', LPAD(FLOOR(RAND() * 60), 2, '0')));
+            -- Insert a random transaction
+            INSERT INTO transactions (customer_id, transaction_type, transaction_value, currency_code, transaction_date)
+            VALUES (
+                       1, -- Using customer ID 1
+                       transaction_type,
+                       ROUND(100 + RAND() * 900, 2), -- Random transaction value between 100 and 1000
+                       ELT(FLOOR(1 + RAND() * 4), 'USD', 'PLN', 'EUR', 'GBP'), -- Random currency code
+                       CONCAT('2024-09-', LPAD(FLOOR(1 + RAND() * 30), 2, '0'), ' ', -- Random day in September 2024
+                              LPAD(FLOOR(RAND() * 24), 2, '0'), ':', LPAD(FLOOR(RAND() * 60), 2, '0'), ':', LPAD(FLOOR(RAND() * 60), 2, '0')) -- Random time
+                   );
 
--- Repeat the above for 599 more rows. You can use a script or loop to generate the exact format for 600 records.
+            -- Increment loop counter
+            SET i = i + 1;
+        END WHILE;
+END$$
+
+DELIMITER ;
+
+-- Call the procedure to insert the records
+CALL insert_random_transactions();
